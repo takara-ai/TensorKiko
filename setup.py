120b847b623bfa942e0762ddf6f41cb895b8cc86
Jordan Legg <149483255+404missinglink@users.noreply.github.com>
2024-10-08 18:21:00 +0100

    update: working build

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/takara-ai/TensorKiko",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
